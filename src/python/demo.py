51f79a9229dcd8bf5f6c81c0f026398d4e798e59
Patrick Philips <jppwriter01@gmail.com>
2019-10-04 15:41:12 -0400

    Added import for CognitiveServicesCredentials

--- src/python/demo.py
+++ src/python/demo.py
@@ -6,14 +6,15 @@ from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import TextOperationStatusCodes
 from azure.cognitiveservices.vision.computervision.models import TextRecognitionMode
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
 
 # Add Computer Vision subscription key and endpoint URL from environment variables
-if 'COGNITIVE_SERVICE_KEY' in os.environ['COGNITIVE_SERVICE_KEY']
+if 'COGNITIVE_SERVICE_KEY' in os.environ:
 	subscription_key = os.environ['COGNITIVE_SERVICE_KEY']
 else:
 	print("\nSet the COMPUTER_VISION_SUBSCRIPTION_KEY environment variable.\n**Restart your shell or IDE for changes to take effect.**")
 
-if 'COMPUTER_VISION_ENDPOINT' in os.environ['COMPUTER_VISION_ENDPOINT']
+if 'COMPUTER_VISION_ENDPOINT' in os.environ:
 	endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
 else:
 	print("\nSet the COMPUTER_VISION_ENDPOINT environment variable.\n**Restart your shell or IDE for changes to take effect.**")
